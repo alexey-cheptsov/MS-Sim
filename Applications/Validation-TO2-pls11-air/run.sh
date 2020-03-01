2fdee19ce3f4fe9e7e3cea9a99cfe54462e8790a
Alexey Cheptsov <cheptsov@gmail.com>
2020-03-01 20:09:24 +0100

    Change to full-mpi map

--- Applications/Validation-TO2-pls11-air/run.sh
+++ Applications/Validation-TO2-pls11-air/run.sh
@@ -2,4 +2,4 @@ export LD_LIBRARY_PATH=$OPEN_MPI_PATH/lib:$LD_LIBRARY_PATH
 export PATH=$OPEN_MPI_PATH/bin:$PATH
 
 mkdir -p output
-mpirun -np 19 --oversubscribe ./test
+mpirun -np 92 --oversubscribe ./test
